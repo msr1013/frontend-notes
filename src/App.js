24e94086f88150d4a5bc2dced234493da8279085
Manish Rawal <manish.rawal@convergetree.com>
2022-03-05 19:31:03 +0545

    implement useEffect to fetch notes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,87 @@
-
+import { useState,useEffect } from "react"
+import axios from 'axios'
 import Note from "./components/Note"
 
-const App = ({notes}) => {
+const App = () => {
+  const [notes,setNotes]=useState([])
+  const [newNote,setNewNote]=useState('')
+  const [showAll,setShowAll] = useState(true)
+
+  const toggleImportanceOf = (id) => {
+    console.log('importance of' + id + 'need to be toggled')
+  }
+
+// useEffect(() => {
+//   axios
+//   .get('http://localhost:3001/api/notes')
+//   .then(res => {
+//     setNotes(res.data)
+//   })
+// },[])
+
+ const hook = () => {
+   console.log('effect')
+   axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+ }
+ useEffect(hook,[])
+
+
+
+  console.log('render',notes.length,'notes')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content:newNote,
+      date:new Date().toISOString(),
+      important:Math.random() <0.5,
+    }
+
+    axios
+    .post('http://localhost:3001/notes',noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+      console.log(response)
+    })
+  }
+
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+  ? notes
+  :notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important':'all'}
+        </button>
+      </div>
       <ul>
-      {notes.map(note =>
-       <Note key={note.id} note={note}/>
+      {notesToShow.map(note =>
+       <Note key={note.id} note={note}
+       toggleImportance={() => toggleImportanceOf(note.id)}
+       />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote}
+        onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }

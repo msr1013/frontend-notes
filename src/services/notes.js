0df64f34684d522f9ff3816038e16ba610cd9948
Manish Rawal <manish.rawal@convergetree.com>
2022-03-08 11:54:04 +0545

    fix axios api call to add update notes

--- src/services/notes.js
+++ src/services/notes.js
@@ -6,5 +6,15 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
+const create = newObject => {
+  const request = axios.post(baseUrl,newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id,newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`,newObject)
+  return request.then(response => response.data)
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {getAll}
+export default {getAll,create,update}

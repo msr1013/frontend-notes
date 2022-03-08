5201010bb4e20dab702e00bbbfcc97270db74a63
Manish Rawal <manish.rawal@convergetree.com>
2022-03-08 11:47:34 +0545

    fix notes importance

--- src/index.js
+++ src/index.js
@@ -4,19 +4,9 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import axios from 'axios'
-
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes=response.data
 
 
-  ReactDOM.render(
-
-      <App notes={notes}/>,
-
-    document.getElementById('root')
-  );
-})
+ReactDOM.render(<App/>,document.getElementById('root'))
 
 
 // If you want to start measuring performance in your app, pass a function
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default {getAll,create,update}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {getAll}

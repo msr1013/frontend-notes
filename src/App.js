18034a06a3861a6bf247fff0cdd32b93e0c009e4
Manish Rawal <manish.rawal@convergetree.com>
2022-03-11 11:58:15 +0545

    implement login ui for users and fetch saved notes by logged in user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-
+import loginService from './services/login'
 import Note from './components/Note'
 import Notification from './components/Notification'
-import Footer from './components/Footer'
+
 import noteService from './services/notes'
 
 const App = () => {
@@ -10,6 +10,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
+  const[user,setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -18,6 +21,28 @@ const App = () => {
         setNotes(initialNotes)
       })
   }, [])
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    try {
+      const user =  loginService.login({
+        username,password
+      })
+
+      noteService.setToken(user.token)
+      console.log(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch(exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      },5000)
+    }
+
+  }
+
 
   const addNote = (event) => {
     event.preventDefault()
@@ -64,34 +89,70 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input type = "text"
+        value={username}
+        name="Username"
+        onChange={({target}) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+        type="password"
+        value={password}
+        name="Password"
+        onChange={({target}) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+      </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+      value={newNote}
+      onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+      </form>
+  )
+
   return (
     <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage} />
+      <h1>
+        Notes
+      </h1>
+      <Notification message={errorMessage}/>
+
+      {user === null ?
+      loginForm() :
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+        <p>{user.name} logged in</p>
+        {noteForm()}
+        </div>
+    }
+
+    <div>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
         </button>
-      </div>
-      <ul>
-        {notesToShow.map(note =>
-          <Note
-            key={note.id}
-            note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)}
-          />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
+    </div>
+    <ul>
+      {notesToShow.map((note) =>
+        <Note
+          key={note.id}
+          note={note}
+          toggleImportance={() => toggleImportanceOf(note.id)}
         />
-        <button type="submit">save</button>
-      </form>
-      <Footer />
+        )}
+    </ul>
+
     </div>
   )
-}
-
-export default App
+      }
+      export default App
